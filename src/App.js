64a7357d578a34e7366bf953771d758f1bb61edf
Jorge Monge <jorge.monge.blanco@outlook.com>
2022-11-12 17:23:59 -0700

    Working fine. Iterate sublayers and disable popups for some of them

--- src/App.js
+++ src/App.js
@@ -1,71 +1,55 @@
 import React, { useRef, useEffect } from "react"
 import MapView from "@arcgis/core/views/MapView"
-import ArcGISMap from "@arcgis/core/Map"
 import WebMap from "@arcgis/core/WebMap"
 import PopupTemplate from "@arcgis/core/PopupTemplate"
 
 import "./App.css"
 
-let uniqueLayer
-
 const App = () => {
   const mapDiv = useRef(null)
 
-  const clickOnView = () => {
-    console.log("click:", uniqueLayer)
-    //uniqueLayer.popup.content = "Hola"
-    uniqueLayer.popup.open()
-  }
-
   useEffect(() => {
-    // this works fine
-    // let map = new ArcGISMap({
-    //   basemap: "topo-vector",
-    // })
-
-    //ine map: 711ea17ce6fa468699d749cb36ac0fb4
-    //redlands map: aa1d3f80270146208328cf66d022e09c
     const map = new WebMap({
       portalItem: {
         id: "711ea17ce6fa468699d749cb36ac0fb4",
       },
-      basemap: "satellite",
+      basemap: "dark-gray-vector",
       apikey: "",
     })
 
-    // eslint-disable-next-line
-    const view = new MapView({
+    const myView = new MapView({
       container: mapDiv.current,
       map,
       center: [-3, 40],
-      zoom: 13,
+      zoom: 5,
     })
 
-    view.when(() => {
-      uniqueLayer = map.layers.items[0]
-      console.log("Unique layer:", uniqueLayer)
+    myView.when(() => {
+      const uniqueLayer = map.layers.items[0]
+      myView.whenLayerView(uniqueLayer).then(() => {
+        // Census sections layer
+        const items = uniqueLayer.allSublayers.items
+        items.forEach((item, index) => {
+          if (index > 0) {
+            item.popupEnabled = false
+          }
+        })
 
-      view.whenLayerView(uniqueLayer).then(() => {
-        const sublayers = uniqueLayer.allSublayers.items
+        const sectionsLevelLyr = uniqueLayer.allSublayers.items[0]
 
-        let sectionsLevelLyr = sublayers[0]
-        console.log("Sections layer:", sectionsLevelLyr)
-        sectionsLevelLyr.popupEnabled = true
         sectionsLevelLyr.popupTemplate = new PopupTemplate({
-          content: "Hola, Carlos",
+          title: "Municipio: {NMUN}",
+          content: [
+            {
+              type: "fields",
+              fieldInfos: [{ fieldName: "NMUN", label: "Municipio:" }],
+            },
+          ],
         })
       })
-
-      //uniqueLayer.popupEnabled = false
-      //uniqueLayer.opacity = 0.5
-      //console.log("Popup template:", uniqueLayer.popupTemplate)
-      // uniqueLayer.popupTemplate = new PopupTemplate({
-      //   content: "Hola",
-      // })
-      //view.on("click", () => clickOnView)
     })
 
-    return () => view && view.destroy()
+    return () => myView && myView.destroy()
   }, [])
 
   return <div className="mapDiv" ref={mapDiv} />
